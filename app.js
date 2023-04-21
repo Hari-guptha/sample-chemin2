df712e88a77e0438148942aa1096f4a25161ad58
Hari-guptha <harihariguptha@gmail.com>
2023-04-21 15:50:17 +0530

    2

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-import express from 'express';
 import http from 'http';
-import { Server } from 'socket.io';
-
+import express from 'express';
 const app = express();
 const server = http.createServer(app);
+import { Server } from 'socket.io';
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-  console.log(`Socket connected: ${socket.id}`);
-
+  console.log('Client connected');
   socket.on('sensorData', (data) => {
-    console.log(`Received sensor data: ${data}`);
-    // Do something with the sensor data here
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`Socket disconnected: ${socket.id}`);
+    console.log('Received sensor data:', data);
+    io.emit('sensorData', data);
   });
 });
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,48 +1,54 @@
-#include <DHT.h>
 #include <ESP8266WiFi.h>
-#include <SocketIOClient.h>
+#include <ArduinoJson.h>
+#include <WebSocketsClient.h>
+#include <DHT.h>  // include the DHT library
 
-#define DHTPIN D4
+#define SSID "Titan"
+#define PASSWORD "12346789"
+#define SERVER "167.172.144.39"
+#define DHTPIN 5 // Changed from D5 to D4
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* ssid = "Titan";
-const char* password = "12346789";
+WebSocketsClient webSocket;
 
-const char* serverName = "167.172.144.39";
-const int serverPort = 4006;
-SocketIOClient socket;
+void setupWiFi() {
+  Serial.println("\nConnecting...");
+
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\nConnected : ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  setupWiFi();
 
-  dht.begin();
+  webSocket.begin(SERVER, 3000, "/socket.io/?EIO=4");
 
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi.");
-
-  socket.begin(serverName, serverPort);
-  socket.on("connect", []() {
-    Serial.println("Connected to server.");
-  });
-  socket.on("disconnect", []() {
-    Serial.println("Disconnected from server.");
-  });
+  dht.begin(); // initialize the DHT sensor
 }
 
 void loop() {
+  webSocket.loop();
+
+  // read the temperature and humidity data from the sensor
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
+
   if (!isnan(temperature) && !isnan(humidity)) {
+    // create a JSON string with the sensor data
     String data = "{\"temperature\":" + String(temperature) + ",\"humidity\":" + String(humidity) + "}";
-    socket.emit("sensorData", data);
+    // send the sensor data to the server
+    webSocket.sendTXT(data);
     Serial.println("Sent data: " + data);
   }
 
-  socket.loop();
-  delay(10000);
+  delay(5000); // wait 5 seconds before sending the next sensor data
 }
